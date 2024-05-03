ed1fdaa8e65c3c9a16b1449b2513b702b3c0a575
Ekaterina Kim <im.work-k@mail.ru>
2024-05-04 00:06:24 +0300

    start project

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,75 @@
-import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
 
 import { AppHeader } from '@components';
+import { ConstructorPage } from '../../pages/constructor-page';
+import { Feed } from '../../pages/feed';
+import { Login } from '../../pages/login';
+import { Register } from '../../pages/register';
+import { ForgotPassword } from '../../pages/forgot-password';
+import { ResetPassword } from '../../pages/reset-password';
+import { Profile } from '../../pages/profile';
+import { ProfileOrders } from '../../pages/profile-orders';
+import { Modal } from '../modal/modal';
+import { OrderInfo } from '../order-info/order-info';
+import { IngredientDetails } from '../ingredient-details/ingredient-details';
+import { NotFound404 } from '../../pages/not-fount-404';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    // <Provider store={store}>
+    <BrowserRouter>
+      <div className={styles.app}>
+        <AppHeader />
+        <ConstructorPage />
+        <Routes>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='Order Info' onClose={handleClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='Ingredients Details' onClose={handleClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='Order Info' onClose={handleClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    // </Provider>
+  );
+};
 
 export default App;

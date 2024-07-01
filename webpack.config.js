7fbe3b2009d27f1f45150c4e8c4290b70c70776b
Ekaterina Kim <im.work-k@mail.ru>
2024-07-01 19:27:05 +0300

    change path

--- webpack.config.js
+++ webpack.config.js
@@ -84,6 +84,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
+    publicPath: '/stellar-burger/'
   },
   devServer: {
     static: path.join(__dirname, './dist'),
